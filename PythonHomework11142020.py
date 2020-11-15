9670edc3b2d1137f0128cc2b69800012618e9837
ViviZ2020 <71572844+ViviZ2020@users.noreply.github.com>
2020-11-14 19:49:05 -0500

    Add files via upload

---
+++ PythonHomework11142020.py
@@ -0,0 +1,9 @@
+def introduce_myself():
+    Name=input('What is your name?')
+    print('Nice to meet you, '+Name+'!')
+    Food=input('What is your favorite food ?')
+    print('I agree that '+Food +' tastes good!')
+    Hobby=input('What is your hobby? ')
+    print('I like '+Hobby+' too!')
+    print('It was nice meeting you, '+Name+' goodbye!')
+introduce_myself()
