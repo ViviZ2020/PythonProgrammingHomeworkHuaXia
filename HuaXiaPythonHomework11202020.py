28f48bf8a6be56ad6f7fca7980615cbd1c268f84
ViviZ2020 <71572844+ViviZ2020@users.noreply.github.com>
2020-11-21 17:12:04 -0500

    Add files via upload

---
+++ HuaXiaPythonHomework11202020.py
@@ -0,0 +1,12 @@
+import turtle
+x=turtle.Turtle()
+x.shape('turtle')
+x.color('lightBlue')
+x.shapesize(1,0.5)
+
+x.forward(100)
+x.right(90)
+x.backward(100)
+x.left(60)
+x.backward(100) 
+x.forward(50)
