09a652082f7c8ff5639c697ad0c92b373914642f
ViviZ2020 <71572844+ViviZ2020@users.noreply.github.com>
2021-03-06 15:43:02 -0500

    Add files via upload

---
+++ PythonHW362021HuaXia.py
@@ -0,0 +1,34 @@
+Planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+
+Letters = (len(Planets[0]))
+int(Letters)
+print(Planets[0], Letters, Letters*2)
+Letters = (len(Planets[1]))
+print(Planets[1], Letters, Letters*2)
+Letters = (len(Planets[2]))
+print(Planets[2], Letters, Letters*2)
+Letters = (len(Planets[3]))
+print(Planets[3], Letters, Letters*2)
+Letters = (len(Planets[4]))
+print(Planets[4], Letters, Letters*2)
+Letters = (len(Planets[5]))
+print(Planets[5], Letters, Letters*2)
+Letters = (len(Planets[6]))
+print(Planets[6], Letters, Letters*2)
+Letters = (len(Planets[7]))
+print(Planets[7], Letters, Letters*2)
+
+
+
+
+
+'''
+Mercury 7 14
+Venus 5 10
+Earth 5 10
+Mars 4 8
+Jupiter 7 14
+Saturn 6 12
+Uranus 6 12
+Neptune 7 14
+'''
